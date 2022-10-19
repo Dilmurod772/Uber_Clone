b92f83d278eee944e7da87957775dcd993e4578f
00014333 <dilmurodbekk892@gmail.com>
2022-10-19 06:21:44 +0500

    Initial Commit

---
+++ Uber_Clone/Uber_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Uber_CloneApp.swift
+//  Uber_Clone
+//
+//  Created by Karimov Dilmurod on 19/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Uber_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
