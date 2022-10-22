ca374e28e1d6095888ca1d18254fcfea14fc1793
00014333 <dilmurodbekk892@gmail.com>
2022-10-22 19:42:23 +0500

    Change Registration Page background color

--- Uber_Clone/utils/Utils.swift
+++ Uber_Clone/utils/Utils.swift
@@ -3,6 +3,6 @@ import Foundation
 import SwiftUI
 
 class Utils {
-    static var color1 = Color(red: 131/255, green: 58/255, blue: 100/255)
-    static var color2 = Color(red: 193/255, green: 53/255, blue: 132/255)
+    static var color2 = Color(red: 252/255, green: 175/255, blue: 69/255)
+    static var color1 = Color(red: 245/255, green: 96/255, blue: 64/255)
 }
